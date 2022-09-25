f6dbf7a00e3359492172fbd47a6b27e75aa51bb6
msnsone <sansone.marcelo@gmail.com>
2022-09-25 17:40:04 -0300

    novo programa insert append for in range

---
+++ lista inclusao exclusao com for append e insert beatles.py
@@ -0,0 +1,25 @@
+﻿# step 1:
+Beatles = []
+print("Step 1:", Beatles)
+
+# step 2:
+
+Beatles.append("John Lennon")
+Beatles.append("Paul McCartney")
+Beatles.append("George Harrison")
+print("Step 2:", Beatles)
+
+# step 3:
+for members in range(2):
+    Beatles.append(input("New band member: "))
+print("Step 3:", Beatles)
+
+# step 4:
+del Beatles[-1]
+del Beatles[-1]
+print("Step 4:", Beatles)
+
+# step 5:
+Beatles.insert(0, "RingoStarr")
+print("Step 5:", Beatles)
+print("The Fab:",len(Beatles))
