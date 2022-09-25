b078492e3a4d7300495b60f011a0a52b9e74522b
msnsone <sansone.marcelo@gmail.com>
2022-09-25 17:07:48 -0300

    novos programas 250922

---
+++ Substituição de numeros em uma lista.py
@@ -0,0 +1,7 @@
+﻿hat_list = [1, 2, 3, 4, 5]  # This is an existing list of numbers hidden in the hat.
+
+n = int(input("Digite um valor inteiro:"))
+hat_list[2] = n
+del hat_list [-1]
+print ("Tamanho da lista: ", len(hat_list))
+print(hat_list)
---
+++ criacao de lista crescente e decrescente usando append e insert.py
@@ -0,0 +1,17 @@
+﻿# COMANDO APPEND CRIA A LISTA DE FORMA CRESCENTE COM fro i in range (n)
+my_list = []  # Creating an empty list.
+
+for i in range(5):
+    my_list.append(i + 1)
+
+print(my_list)
+
+# COMANDO INSERT CRIA A LISTA DE FORMA DECRESCENTE /COM for i in range (n)
+
+my_list = []  # Creating an empty list.
+
+for i in range(5):
+    my_list.insert(0, i + 1)
+
+print(my_list)
+
