622b3a219af7e958c013e98262692fb80abf36d6
msnsone <sansone.marcelo@gmail.com>
2022-09-28 02:25:41 -0300

    update de programas

---
+++ ano bissexto.py
@@ -0,0 +1,11 @@
+﻿year = int(input("Enter a year: "))
+if year < 1582:
+    print ("Este ano não faz parte do calendário Gregoriano!")
+elif year % 4 != 0:
+ print ("o ano", year, "é um ano comum")
+elif year % 100 != 0:
+    print ("o ano", year, "é um ano bissexto")
+elif year % 400 != 0:
+    print ("o ano", year, "é um ano comum")
+else:
+    print ("o ano", year, "é um ano bissexto")
---
+++ comedor de vogais.py
@@ -0,0 +1,20 @@
+﻿word_without_vowels = ""
+
+user_word = input("Enter your word: ")
+user_word = user_word.upper()
+
+for letter in user_word:
+    if letter == "A":
+        continue
+    elif letter == "E":
+        continue
+    elif letter == "I":
+        continue
+    elif letter == "O":
+        continue
+    elif letter == "U":
+        continue
+    else:
+        word_without_vowels += letter
+		
+print(word_without_vowels)
