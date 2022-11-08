cefee001c2098aea2f62961717841aca3e4c7a08
msnsone <sansone.marcelo@gmail.com>
2022-11-08 15:21:37 -0300

    Create exemplo if-elif-else.py

---
+++ exemplo if-elif-else.py
@@ -0,0 +1,8 @@
+﻿name = input("Enter flower name: ")
+
+if name == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+elif name == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+else:
+    print("Spathiphyllum! Not", name + "!")
