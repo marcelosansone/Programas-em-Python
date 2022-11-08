9b3481aef26d2ae046b84fc31c513f95b967c0c6
msnsone <sansone.marcelo@gmail.com>
2022-11-08 15:24:48 -0300

    Create Exemplo if else com cálculo de taxas condicionais.py

---
+++ Exemplo if else com cálculo de taxas condicionais.py
@@ -0,0 +1,12 @@
+﻿income = float(input("Enter the annual income: "))
+
+if income < 85528:
+	tax = income * 0.18 - 556.02
+else:
+	tax = (income - 85528) * 0.32 + 14839.02
+
+if tax < 0.0:
+	tax = 0.0
+
+tax = round(tax, 0)
+print("The tax is:", tax, "thalers")
