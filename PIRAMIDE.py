4cc26b05727980491a6c08e790b839bc1641bc0b
msnsone <sansone.marcelo@gmail.com>
2022-11-11 21:25:03 -0300

    UPDATE

---
+++ PIRAMIDE.py
@@ -0,0 +1,10 @@
+﻿blocks = int(input("Enter the number of blocks: "))
+
+height = 0
+in_layer = 1
+while in_layer <= blocks:
+    height += 1
+    blocks -= in_layer
+    in_layer += 1
+
+print("The height of the pyramid:", height)
