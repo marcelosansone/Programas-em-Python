7ed3b5506d9520c94e8819127173b1ee714fff13
msnsone <sansone.marcelo@gmail.com>
2022-11-22 20:07:00 -0300

    curso python

---
+++ main.py
@@ -0,0 +1,5 @@
+﻿while True:
+    word = input("You're stuck in an infinite loop!\nEnter the secret word to leave the loop: ")
+    if word == "chupacabra":
+        break
+print("You've successfully left the loop!")
---
+++ n = 3.py
@@ -0,0 +1,7 @@
+n = 3
+
+while n > 0:
+    print(n + 1)
+    n -= 1
+else:
+    print(n)
