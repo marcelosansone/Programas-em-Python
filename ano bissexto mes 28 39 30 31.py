00b8ea9d0e1d4e7fecffe59baa5bb2b86f852c22
Marcelo Sansone <sansone.marcelo@gmail.com>
2023-01-04 15:16:03 -0300

    exercicios curso

---
+++ ano bissexto mes 28 39 30 31.py
@@ -0,0 +1,31 @@
+﻿def is_year_leap(year):
+	if year % 4 != 0:
+		return False
+	elif year % 100 != 0:
+		return True
+	elif year % 400 != 0:
+		return False
+	else:
+		return True
+
+def days_in_month(year,month):
+	if year < 1582 or month < 1 or month > 12:
+		return None
+	days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+	res  = days[month - 1]
+	if month == 2 and is_year_leap(year):
+		res = 29
+	return res
+
+test_years = [1900, 2000, 2016, 1987]
+test_months = [ 2, 2, 1, 11]
+test_results = [28, 29, 31, 30]
+for i in range(len(test_years)):
+	yr = test_years[i]
+	mo = test_months[i]
+	print(yr,mo,"-> ",end="")
+	result = days_in_month(yr, mo)
+	if result == test_results[i]:
+		print("OK")
+	else:
+		print("Failed")
---
+++ funcao galao milha litro kilometro.py
@@ -0,0 +1,19 @@
+﻿# 1 American mile = 1609.344 metres
+# 1 American gallon = 3.785411784 litres
+
+def liters_100km_to_miles_gallon(litres):
+    gallons = litres / 3.785411784
+    miles = 100 * 1000 / 1609.344
+    return miles / gallons
+
+def miles_gallon_to_liters_100km(miles):
+    km100 = miles * 1609.344 / 1000 / 100
+    litres = 3.785411784
+    return litres / km100
+
+print(liters_100km_to_miles_gallon(3.9))
+print(liters_100km_to_miles_gallon(7.5))
+print(liters_100km_to_miles_gallon(10.))
+print(miles_gallon_to_liters_100km(60.3))
+print(miles_gallon_to_liters_100km(31.4))
+print(miles_gallon_to_liters_100km(23.5))
