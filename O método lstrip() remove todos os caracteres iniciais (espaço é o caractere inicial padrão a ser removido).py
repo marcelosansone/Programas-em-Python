9f719204a21c89995d3f427ab8ebc7bc0d9f480e
Marcelo Sansone <sansone.marcelo@gmail.com>
2023-01-10 10:55:25 -0300

    update

---
+++ O método lstrip() remove todos os caracteres iniciais (espaço é o caractere inicial padrão a ser removido).py
@@ -0,0 +1,6 @@
+﻿# Demonstrating the lstrip() method:
+print("[" + " tau ".lstrip() + "]")
+print("pythoninstitute.org".lstrip(".org"))
+
+print("www.cisco.com".lstrip("w."))
+
---
+++ isalnum () isalpha isnum().py
@@ -0,0 +1,17 @@
+﻿# Demonstrating the isalnum() method:
+print('lambda30'.isalnum())
+print('lambda'.isalnum())
+print('30'.isalnum())
+print('@'.isalnum())
+print('lambda_30'.isalnum())
+print(''.isalnum())
+
+
+
+# Example 1: Demonstrating the isapha() method:
+print("Moooo".isalpha())
+print('Mu40'.isalpha())
+
+# Example 2: Demonstrating the isdigit() method:
+print('2018'.isdigit())
+print("Year2019".isdigit())
