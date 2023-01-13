e3dc9e58f53cd99b91f60a1d93714efdb96c8f01
Marcelo Sansone <sansone.marcelo@gmail.com>
2023-01-13 17:46:04 -0300

    update130123

---
+++ Como criar a sua própria exceção.py
@@ -0,0 +1,24 @@
+﻿class MyZeroDivisionError(ZeroDivisionError):	
+    pass
+
+
+def do_the_division(mine):
+    if mine:
+        raise MyZeroDivisionError("some worse news")
+    else:		
+        raise ZeroDivisionError("some bad news")
+
+
+for mode in [False, True]:
+    try:
+        do_the_division(mode)
+    except ZeroDivisionError:
+        print('Division by zero')
+
+for mode in [False, True]:
+    try:
+        do_the_division(mode)
+    except MyZeroDivisionError:
+        print('My division by zero')
+    except ZeroDivisionError:
+        print('Original division by zero')
---
+++ exemplo de utilizacao funcao lambda no lugar de funcao que nao sera mais utilizada.py
@@ -0,0 +1,18 @@
+﻿def print_function(args, fun):
+    for x in args:
+        print('f(', x,')=', fun(x), sep='')
+
+
+def poly(x):
+    return 2 * x**2 - 4 * x + 2
+
+
+print_function([x for x in range(-2, 3)], poly)
+
+#exemplo usando lmbda, funcao não será usada novamente
+def print_function(args, fun):
+    for x in args:
+        print('f(', x,')=', fun(x), sep='')
+
+print_function([x for x in range(-2, 3)], lambda x: 2 * x**2 - 4 * x + 2)
+
